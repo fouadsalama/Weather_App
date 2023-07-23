3a16ccd71b071f16fd526b5caba9dd7f72052a95
Fouad Salama <fs2030565@gmail.com>
2023-07-23 04:02:05 +0300

    finish weather app

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,23 @@ import 'package:weather_app/cubits/weather_cubit.dart';
 import 'package:weather_app/pages/home_page.dart';
 
 import 'package:weather_app/services/weather_service.dart';
+import 'package:weather_app/simple_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = SimpleObserver();
   runApp(BlocProvider(
       create: (context) {
         return WeatherCubit(WeatherService());
       },
-      child: WeatherApp()));
+      child: const WeatherApp()));
 }
 
 class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // theme: ThemeData(
-      //   primarySwatch: BlocProvider.of<WeatherCubit>(context).color,
-      // ),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
